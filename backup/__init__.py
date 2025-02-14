39048c1dc7a14efc602151dddc5f897945cf723f
Ardusa <ardusa05@gmail.com>
2025-02-14 12:19:21 -0500

    Remove backup functionality and related models from the project

--- backup/__init__.py
+++
@@ -1,3 +0,0 @@
-from .backup import backup_sqlite_with_api
-
-__all__ = ["backup_sqlite_with_api"]
--- services/__init__.py
+++ services/__init__.py
@@ -1,7 +1,7 @@
 """ This module is used to import all the services in the application. """
 
 from .auth_service import create_access_token, create_refresh_token, decode_jwt
-from .db_service import get_db, fetch_table_data, save_table_to_file, custom_serializer, convert_to_json, convert_uuid_keys_to_str
+from .db_service import get_db, fetch_table_data, save_table_to_file, custom_serializer, convert_to_json, convert_uuid_keys_to_str, backup_sqlite_with_api
 from .email_service import send_email
 from .account_service import load_account, create_account, authenticate_account, load_accounts
 from .project_service import load_project, create_project, load_projects, update_project, delete_project
@@ -36,4 +36,5 @@ __all__ = [
     "fetch_logo",
     "create_company",
     "create_company_with_details",
+    "backup_sqlite_with_api"
 ]
